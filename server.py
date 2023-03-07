4232072badcc33bf6f6e2888ace150a67358caca
SHQ <8694069+sunhq6@user.noreply.gitee.com>
2023-03-07 15:00:13 +0800

    修改：     server.py

--- server.py
+++ server.py
@@ -21,6 +21,7 @@ def handle_client(client_socket, client_addr):
     clients.append(client_socket)
     while True:
         msg = client_socket.recv(4096)
+        print(f"msg from{client_addr}: {msg}")
         broadcast(msg, client_socket)
 
 
